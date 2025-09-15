59c56c9534480098d30a4c6cdeed43f2d24b98a5
will-xujun <willxujun@hotmail.com>
2025-09-15 16:21:12 +0800

    change to algebra library

--- Util.thy
+++ Util.thy
@@ -1,15 +1,6 @@
 theory Util
   imports Complex_Main
+    "HOL-Algebra.Algebra"
 begin
-
-lemma lfp_preserve:
-  fixes W:: "'a set"
-    and f:: "'a set \<Rightarrow> 'a set"
-  assumes "\<forall>A. f A \<subseteq> W"
-  shows "lfp f \<subseteq> W"
-  apply (simp add:lfp_def)
-proof - 
-  show " \<Inter> {u. f u \<subseteq> u} \<subseteq> W " using assms by blast
-qed
 
 end
